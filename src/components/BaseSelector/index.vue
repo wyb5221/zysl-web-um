1da50d8238b8f3318fd2708a5d1cde1ea1986308
zoufa <295957459@qq.com>
2019-11-06 16:53:32 +0800

    微服务管理界面优化

---
+++ src/components/BaseSelector/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-select v-model="selectItemValue" @change="selectChangedFn" placeholder="请选择" class="el-input el-input--small">
+      <el-option
+        v-for="item in options"
+        :key="item.id"
+        :label="item.name"
+        :value="item.id"
+      >
+      </el-option>
+    </el-select>
+</template>
+
+<script>
+    export default {
+        name: 'BaseSelector',
+        props: {
+            options: {
+                type: Array,
+                default: () => [{
+                    id: '1',
+                    name: 'test1'
+                }, {
+                    id: '2',
+                    name: 'test2'
+                }]
+            }
+        },
+        data() {
+            return {
+                selectItemValue: '',
+                bindItems: []
+            }
+        },
+        methods: {
+            initData() {
+                this.bindItems = this.options
+                this.$emit('selectChangeEvent', this.selectItemValue)
+            },
+            selectChangedFn(){
+                this.$emit('selectChangeEvent',this.selectItemValue)
+            },
+            setSelectOptionNum(sNum) {
+                this.selectItemValue = sNum;
+            },
+
+        },
+        created() {
+            this.initData()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
